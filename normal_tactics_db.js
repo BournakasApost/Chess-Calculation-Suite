565edc3c55b9d570c4353ab7d5ad1840286657e6
BournakasApost <BournakasAp@proton.me>
2026-04-15 15:28:49 +0300

    Update normal_tactics_db.js

--- normal_tactics_db.js
+++ normal_tactics_db.js
@@ -1,474 +1,200 @@
 window.NORMAL_TACTICS = [
     {
-        id: 'nt-1',
-        difficulty: 0.5,
+        id: 'tactic_1',
+        rating: 630,
         theme: 'Protecting',
         fen: '8/3r1p2/pk4p1/1p6/2pNP3/5P2/PPP1B1P1/5K2 w - - 0 1',
         solution: 'c3' 
     },
     {
-        id: 'nt-2',
-        difficulty: 0.5,
+        id: 'tactic_2',
+        rating: 640,
         theme: 'Protecting',
         fen: '6k1/4b1p1/3p3p/5n2/8/2PB2PP/2P2P2/6K1 b - - 0 1',
         solution: 'g6' 
     },
     {
-        id: 'nt-3',
-        difficulty: 0.7,
+        id: 'tactic_3',
+        rating: 650,
         theme: 'Protecting',
         fen: 'k3b3/ppn5/5p2/1P3Pp1/P3K3/8/8/2R5 b - - 0 1',
         solution: 'Kb8' 
     },
     {
-        id: 'nt-4',
-        difficulty: 0.5,
+        id: 'tactic_4',
+        rating: 620,
         theme: 'Protecting',
         fen: '2n5/5kpp/2p5/prB1p3/8/P3PP2/K5PP/5R2 w - - 0 1',
         solution: 'Rc1' 
     },
     {
-        id: 'nt-5',
-        difficulty: 0.5,
+        id: 'tactic_5',
+        rating: 630,
         theme: 'Protecting',
         fen: '4k3/p3n3/1p3bp1/4N2p/2P1p3/1PP3P1/5BKP/8 w - - 0 1',
         solution: 'Bd4' 
     },
     {
-        id: 'nt-6',
-        difficulty: 0.5,
+        id: 'tactic_6',
+        rating: 660,
         theme: 'Protecting',
         fen: '8/1p1k1p1p/p2pbn2/4p3/4P3/P7/1PPB2PP/1N2K3 w - - 0 1',
         solution: 'Nc3' 
     },
     {
-        id: 'nt-7',
-        difficulty: 0.5,
+        id: 'tactic_7',
+        rating: 650,
+        theme: 'Protecting',
+        fen: '6k1/1p3pp1/1p6/pP1b1P1p/6n1/2K5/3R2P1/4R3 b - - 0 1',
+        solution: 'Nf6' 
+    },
+    {
+        id: 'tactic_8',
+        rating: 640,
         theme: 'Protecting',
         fen: '1k1r4/pp6/1np2p2/6p1/3Pb1P1/4PN2/P2B1P1P/4R1K1 w - - 0 1',
         solution: 'Kg2' 
     },
     {
-        id: 'nt-8',
-        difficulty: 0.5,
+        id: 'tactic_9',
+        rating: 630,
         theme: 'Protecting',
         fen: '2r2nk1/2P3p1/p7/8/8/P1N2RK1/8/8 w - - 0 1',
         solution: 'Nd5' 
     },
     {
-        id: 'nt-9',
-        difficulty: 0.5,
+        id: 'tactic_10',
+        rating: 670,
         theme: 'Protecting',
         fen: '8/1pk1b3/2p2p1p/7P/5R1n/1P6/P7/5K2 b - - 0 1',
         solution: 'f5' 
     },
     {
-        id: 'nt-10',
-        difficulty: 0.5,
+        id: 'tactic_11',
+        rating: 680,
         theme: 'Protecting',
         fen: 'r7/2p1k2p/4p1n1/1pP1Ppp1/1P1R4/P5P1/1B1K3P/8 w - - 0 1',
         solution: 'Rd3' 
     },
     {
-        id: 'nt-11',
-        difficulty: 0.5,
+        id: 'tactic_12',
+        rating: 690,
         theme: 'Protecting',
         fen: 'r7/3k4/2p3bp/PpPp2p1/3P4/2N2P1P/P5P1/3KB3 w - - 0 1',
         solution: 'Ne2' 
     },
     {
-        id: 'nt-12',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '8/4k2p/1pp3n1/8/2N5/P4PP1/4K3/8 b - - 0 1',
-        solution: 'b5' 
-    },
-    {
-        id: 'nt-13',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '8/p5k1/1p1r1pp1/1P6/2Pp4/3K2BB/5P2/8 b - - 0 1',
-        solution: 'Rd8' 
-    },
-    {
-        id: 'nt-14',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '5r2/8/2k3np/2pbR2p/3p4/3P2P1/2PNN2P/4K3 w - - 0 1',
-        solution: 'Rxh5' 
-    },
-    {
-        id: 'nt-15',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '8/p1p1k2p/P2n1pp1/P1bR4/8/1K3P1N/6P1/8 b - - 0 1',
-        solution: 'Be3' 
-    },
-    {
-        id: 'nt-16',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '8/p1p1k3/4p2p/4P1b1/2NP3P/1P4K1/8/8 b - - 0 1',
-        solution: 'Bc1' 
-    },
-    {
-        id: 'nt-17',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '8/6k1/1p6/q3p3/2B1P3/2P2P2/R2K4/8 b - - 0 1',
-        solution: 'Qc5' 
-    },
-    {
-        id: 'nt-18',
-        difficulty: 0.5,
-        theme: 'Moving away',
-        fen: '7r/1p2kp2/1pb2pp1/n2Q4/3P1N1P/6P1/1P1K4/8 w - - 0 1',
-        solution: 'Qa2' 
-    },
-    {
-        id: 'nt-19',
-        difficulty: 0.9,
-        theme: 'Moving away',
-        fen: '4r3/1k4bp/1p2b1p1/2p5/2P2p2/1P2N3/2P2PKP/3R1B2 w - - 0 1',
-        solution: 'Nd5' 
-    },
-    {
-        id: 'nt-20',
-        difficulty: 0.8,
-        theme: 'Moving away',
-        fen: '8/1np3kp/5pp1/3Q4/1p6/1P5P/4r1PK/8 b - - 0 1',
-        solution: 'Nd6' 
-    },
-    {
-        id: 'nt-21',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '4k3/8/4np2/4p3/2B1P3/8/5P2/4K3 w - - 0 1',
-        solution: 'Bxe6' 
-    },
-    {
-        id: 'nt-22',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '6k1/2p1b1pp/1pn5/4p1B1/8/P1N1P3/1P3PP1/4K3 b - - 0 1',
-        solution: 'Bxg5' 
-    },
-    {
-        id: 'nt-23',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r7/5kp1/5p2/3b4/p7/P1N3P1/5P2/4R1K1 w - - 0 1',
-        solution: 'Nxd5' 
-    },
-    {
-        id: 'nt-24',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r7/8/3k4/p2p4/3Pn3/2P3P1/3B2K1/2R5 b - - 0 1',
-        solution: 'Nxd2' 
-    },
-    {
-        id: 'nt-25',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '4r1k1/1p3pp1/2pn4/8/7P/5BP1/1P3PK1/3R4 w - - 0 1',
-        solution: 'Rxd6' 
-    },
-    {
-        id: 'nt-26',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '8/r4pkp/4np2/2p5/8/BP6/2P2PP1/4R1K1 b - - 0 1',
-        solution: 'Rxa3' 
-    },
-    {
-        id: 'nt-27',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '7k/4q1p1/2p4r/rp6/8/7P/2PQ2P1/3R1RK1 w - - 0 1',
-        solution: 'Qxa5' 
-    },
-    {
-        id: 'nt-28',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '4n1k1/1p3qbp/2p5/2Q4N/8/1P5P/P5PK/5B2 b - - 0 1',
-        solution: 'Qxf1' 
-    },
-    {
-        id: 'nt-29',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '6k1/5pp1/8/1p6/4n3/1B3KP1/1BP2b1P/8 w - - 0 1',
-        solution: 'Kxe4' 
-    },
-    {
-        id: 'nt-30',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '3r1bk1/5Npp/8/1pp5/8/1P6/2P2PPP/4R1K1 b - - 0 1',
-        solution: 'Kxf7' 
-    },
-    {
-        id: 'nt-31',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '8/1p2npk1/6p1/3Np3/8/4P1P1/1P3PK1/8 b - - 0 1',
-        solution: 'Nxd5' 
-    },
-    {
-        id: 'nt-32',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '2r5/pk4p1/1p4Rb/3b4/4P3/1NP4P/1PK5/8 w - - 0 1',
-        solution: 'exd5' 
-    },
-    {
-        id: 'nt-33',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r2k4/8/6p1/1pPn1p2/4B3/1P6/3RK3/8 b - - 0 1',
-        solution: 'fxe4' 
-    },
-    {
-        id: 'nt-34',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '7r/1k5p/1p4p1/2b5/2PB3P/6P1/2P3K1/1R6 b - - 0 1',
-        solution: 'Bxd4' 
-    },
-    {
-        id: 'nt-35',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '2b5/6kp/5np1/8/8/2r2P2/5PK1/2R4R b - - 0 1',
-        solution: 'Rxc1' 
-    },
-    {
-        id: 'nt-36',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '8/1k5p/2p1prp1/4b3/6P1/2B4P/1P4K1/7R b - - 0 1',
-        solution: 'Bxc3' 
-    },
-    {
-        id: 'nt-37',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '5k2/4r3/1b6/5p2/1p3p2/1P5P/3NRP2/1K6 w - - 0 1',
-        solution: 'Rxe7' 
-    },
-    {
-        id: 'nt-38',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '3q4/4b1pk/7p/4p1B1/4P2P/4Q1P1/8/7K w - - 0 1',
-        solution: 'Bxe7' 
-    },
-    {
-        id: 'nt-39',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '8/2p5/1prk1p2/6n1/1P6/1P2RB2/1K4P1/8 b - - 0 1',
-        solution: 'Nxf3' 
-    },
-    {
-        id: 'nt-40',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '3q3r/5pk1/1p3p2/3Q4/8/1P4P1/P2B1PK1/8 w - - 0 1',
-        solution: 'Qxd8' 
-    },
-    {
-        id: 'nt-41',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '8/1p3k2/2p1n1pb/5p2/2PNp3/1P4P1/2B1KP1P/8 w - - 0 1',
-        solution: 'Nxe6' 
-    },
-    {
-        id: 'nt-42',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '4r1k1/3bRpp1/3p1n1p/8/2PP4/R1K3P1/7P/8 w - - 0 1',
-        solution: 'Rxe8+' 
-    },
-    {
-        id: 'nt-44',
-        difficulty: 0.6,
-        theme: 'Capturing',
-        fen: 'r6r/ppp1kppp/2p3n1/2b5/2BPP3/2P2N2/P3KPPP/R6R w - - 0 1',
-        solution: 'dxc5' 
-    },
-    {
-        id: 'nt-45',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r2qr2k/1pp3pp/3b4/8/2P5/P1Q5/4N1PP/1R3R1K b - - 0 1',
-        solution: 'Rxe2' 
-    },
-    {
-        id: 'nt-46',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r1b2r2/1ppk2pp/1b1p2n1/3B4/4P3/2PN1N2/2K2P1P/1R5R b - - 0 1',
-        solution: 'Rxf3' 
-    },
-    {
-        id: 'nt-47',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '3rr3/1bp2kpp/1p3pn1/3P4/1R6/2PN1q2/2KQ1P1P/5B1R b - - 0 1',
-        solution: 'Qxh1' 
-    },
-    {
-        id: 'nt-48',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: '1r3r1k/p1p1bqpp/2b5/8/3N4/6N1/P2Q1PPP/R4RK1 w - - 0 1',
-        solution: 'Nxc6' 
-    },
-    {
-        id: 'nt-49',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r3k2r/1p2qppp/p1n2n2/1Bb1p3/4P3/5N2/PPQ2PPP/R1B1K2R b - - 0 1',
-        solution: 'axb5',
-        alternatives: {0:'Bb4+'} 
-    },
-    {
-        id: 'nt-50',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'k3q3/1p3p2/p5pr/8/R3P3/1P6/P2Q3P/4K3 w - - 0 1',
-        solution: 'Qxh6' 
-    },
-    {
-        id: 'nt-51',
-        difficulty: 0.8,
-        theme: 'Capturing',
-        fen: 'r2qk2r/1b2bppp/p3p3/1p1nP3/3pNB2/3B1N2/PP3PPP/R2Q1RK1 b - - 0 1',
-        solution: 'Nxf4' 
-    },
-    {
-        id: 'nt-52',
-        difficulty: 0.5,
-        theme: 'Capturing',
-        fen: 'r2r2k1/2q1bp1p/2pN2p1/pp2P3/4bP1n/P1B5/1PP1BQPP/1K1R3R w - - 0 1',
-        solution: 'Nxe4' 
-    },
-    {
-        id: 'nt-53',
-        difficulty: 0.8,
-        theme: 'Capturing',
-        fen: 'r4rk1/p2R1p1p/1p4p1/2p4n/2B3q1/P3Q1P1/1PPN1P1P/1K5R b - - 0 1',
-        solution: 'Qxd7' 
-    },
-    {
-        id: 'nt-43',
-        difficulty: 8,
+        id: 'tactic_13',
+        rating: 2330,
         theme: 'Composition: Mate in 2',
         fen: '8/8/8/8/1Q6/1K6/8/2Nk4 w - - 0 1',
-        solution: ['Qa5', 'Kxc1', 'Qe1#'] 
+        solution: ['Qa5','Kxc1','Qe1#'] 
     },
-    {
-        id: 'nt-54',
-        difficulty: 8,
+        {
+        id: 'tactic_14',
+        rating: 2350,
         theme: 'Composition: Mate in 2',
         fen: '8/8/5R2/8/2P1k3/2K5/5P2/2B5 w - - 0 1',
-        solution: ['Bb2', 'Ke5', 'Kd3#'] 
+        solution: ['Bb2','Ke5','Kd3#'] 
     },
     {
-        id: 'nt-55',
-        difficulty: 8.5,
-        theme: 'Composition: Mate in 2',
-        fen: '2b4Q/4p2K/4pr2/2P1k3/2P2R2/3P3p/2n4B/8 w - - 0 1',
-        solution: ['Kg7', 'Rxf4', 'Kg6#'] 
-    },
-    {
-        id: 'nt-56',
-        difficulty: 8,
+        id: 'tactic_15',
+        rating: 2335,
         theme: 'Composition: Mate in 2',
         fen: '2B5/2p1B1nn/2PR2p1/1K2k3/1p2p3/2b1P3/5R2/8 w - - 0 1',
-        solution: ['Kc5', 'cxd6+', 'Bxd6#'] 
+        solution: ['Kc5','cxd6+','Bxd6#'] 
     },
     {
-        id: 'nt-57',
-        difficulty: 8,
+        id: 'tactic_16',
+        rating: 2370,
+        theme: 'Composition: Mate in 2',
+        fen: '2b4Q/4p2K/4pr2/2P1k3/2P2R2/3P3p/2n4B/8 w - - 0 1',
+        solution: ['Kg7','Rxf4','Kg6#'] 
+    },
+    {
+        id: 'tactic_17',
+        rating: 2350,
         theme: 'Composition: Mate in 2',
         fen: '8/2R5/8/B2N4/p3B3/Q1pP4/1pqk1K2/br1b3R w - - 0 1',
-        solution: ['Nxc3', 'Kc1+', 'Ne2#'] 
+        solution: ['Nxc3','Kc1+','Ne2#'] 
     },
     {
-        id: 'nt-58',
-        difficulty: 2.8,
+        id: 'tactic_18',
+        rating: 1600,
         theme: 'Mating Attack',
         fen: '3qr1k1/pbr1bp1p/1pn1p1pB/8/2BP1QN1/2P3P1/P4P1P/2R1R1K1 w - - 0 1',
         solution: ['Qxf7+','Kxf7','Bxe6#'] 
     },
     {
-        id: 'nt-59',
-        difficulty: 2.5,
-        theme: 'Mating Attack',
-        fen: '1r3r2/p4p1k/2q2Pnp/2pppR2/2p5/2P5/P2QB1PP/R5K1 w - - 0 1',
-        solution: ['Qxh6+','Kxh6','Rh5#'] 
-    },
-    {
-        id: 'nt-60',
-        difficulty: 2.7,
-        theme: 'Mating Attack',
-        fen: '2krqb1r/pp1n1pp1/2p2n1p/4B3/2BP4/P1N2Q2/1PP3PP/R3R1K1 w - - 0 1',
-        solution: ['Qxc6+','bxc6','Ba6#'] 
-    },
-    {
-        id: 'nt-61',
-        difficulty: 2.6,
-        theme: 'Mating Attack',
-        fen: 'r1b1k2r/ppqn1pp1/2p1p2p/6B1/2PPR3/3B1Q1P/PP3PP1/R5K1 w kq - 0 1',
-        solution: ['Rxe6+','fxe6','Bg6#'] 
-    },
-    {
-        id: 'nt-62',
-        difficulty: 2.4,
+        id: 'tactic_19',
+        rating: 1550,
         theme: 'Mating Attack',
         fen: '2r2rk1/q4pp1/p2Rp2p/1p2Pn2/4QPn1/P6N/1P4PP/1B4RK b - - 0 1',
         solution: ['Qxg1+','Nxg1','Nf2#'] 
     },
     {
-        id: 'nt-63',
-        difficulty: 3.8,
+        id: 'tactic_20',
+        rating: 1450,
+        theme: 'Mating Attack',
+        fen: '1r3r2/p4p1k/2q2Pnp/2pppR2/2p5/2P5/P2QB1PP/R5K1 w - - 0 1',
+        solution: ['Qxh6+','Kxh6','Rh5#'] 
+    },
+    {
+        id: 'tactic_21',
+        rating: 1430,
+        theme: 'Mating Attack',
+        fen: '2krqb1r/pp1n1pp1/2p2n1p/4B3/2BP4/P1N2Q2/1PP3PP/R3R1K1 w - - 0 1',
+        solution: ['Qxc6+','bxc6','Ba6#'] 
+    },
+    {
+        id: 'tactic_22',
+        rating: 1600,
+        theme: 'Mating Attack',
+        fen: 'r1b1k2r/ppqn1pp1/2p1p2p/6B1/2PPR3/3B1Q1P/PP3PP1/R5K1 w kq - 0 1',
+        solution: ['Rxe6+','fxe6','Bg6#'] 
+    },
+    {
+        id: 'tactic_22',
+        rating: 1600,
+        theme: 'Mating Attack',
+        fen: 'r1b1k2r/ppqn1pp1/2p1p2p/6B1/2PPR3/3B1Q1P/PP3PP1/R5K1 w kq - 0 1',
+        solution: ['Rxe6+','fxe6','Bg6#'] 
+    },
+    {
+        id: 'tactic_23',
+        rating: 1700,
         theme: 'Decoy',
         fen: 'r1bqr1k1/5p2/3p1bpp/p2P4/1R2P3/1P3PN1/R2QB1PP/7K b - - 0 4',
         solution: ['Bc3','Qxc3','axb4'] 
     },
     {
-        id: 'nt-64',
-        difficulty: 3.1,
+        id: 'tactic_24',
+        rating: 1430,
         theme: 'Decoy',
         fen: '2n5/p5bp/1pb1k3/2p1pp2/P1N5/1PB1NPPP/2P1K3/8 b - - 0 1',
-        solution: ['Bxf3+','Kxf3','Bxc3'] 
+        solution: ['Bxf3+','Kxf3','e4+','Ke2','Bxc3'] 
     },
     {
-        id: 'nt-65',
-        difficulty: 3.1,
+        id: 'tactic_25',
+        rating: 1390,
         theme: 'Decoy',
         fen: '8/1p6/p3kn1r/4pp2/1PP5/6NP/P5K1/3R4 w - - 0 1',
-        solution: ['Rd6+','Kxd6','Nxf5+'] 
+        solution: ['Rd6+','Kxd6','Nxf5+','Ke6','Nxh6'] 
     },
     {
-        id: 'nt-66',
-        difficulty: 3.1,
+        id: 'tactic_26',
+        rating: 1550,
         theme: 'Decoy',
         fen: '2r4k/3r1p1p/1p2pP2/p2pPp1P/P2P1Q2/6R1/4B1PK/2q5 w - - 0 1',
         solution: ['Rg8+','Kxg8','Qg3+','Kf8','Qg7+','Ke8','Qg8#'],
-        alternatives:{6:'Qh8#'} 
+        alternatives: {6:['Qh8#']} 
     },
     {
-        id: 'nt-67',
-        difficulty: 3.1,
+        id: 'tactic_27',
+        rating: 1350,
         theme: 'Decoy',
         fen: '2q5/pR6/1p3pnk/1P4pp/8/5QPP/P2r2BK/8 w - - 0 1',
         solution: ['Qxh5+','Kxh5','Rh7#'] 
     },
-    
+
 ];
